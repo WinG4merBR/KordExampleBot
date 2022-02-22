b4b079b4c6578a2a54b06c5b69a9414c17cb88ad
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2022-02-21 22:37:42 -0300

    xu

--- src/main/java/com/wing4merbr/discordbot/BotClient.java
+++ src/main/java/com/wing4merbr/discordbot/BotClient.java
@@ -1,22 +1,18 @@
 package com.wing4merbr.discordbot;
 
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.JDAInfo;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-
 import javax.security.auth.login.LoginException;
 
 public class BotClient extends ListenerAdapter
 {
-    public static void main(String[] args) throws LoginException
-    {
+    public static void main(String[] args) throws LoginException {
         JDABuilder.createLight("bottoken", GatewayIntent.GUILD_MESSAGES)
                 .addEventListeners(new BotClient())
                 .setActivity(Activity.playing("uwu"))
