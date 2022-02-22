8b24fa580995ae4e1493f761279f4e544105ba44
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2022-02-21 22:32:30 -0300

    Algumas mudanças sla

--- src/main/java/com/wing4merbr/discordbot/BotClient.java
+++ src/main/java/com/wing4merbr/discordbot/BotClient.java
@@ -1,18 +1,39 @@
 package com.wing4merbr.discordbot;
 
 import net.dv8tion.jda.api.JDABuilder;
-import java.io.IOException;
-import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.JDAInfo;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
-public class BotClient {
-    public static void main(String[] args) throws IOException, LoginException {
-        JDABuilder client = JDABuilder.createDefault(token);
+public class BotClient extends ListenerAdapter
+{
+    public static void main(String[] args) throws LoginException
+    {
+        JDABuilder.createLight("bottoken", GatewayIntent.GUILD_MESSAGES)
+                .addEventListeners(new BotClient())
+                .setActivity(Activity.playing("uwu"))
+                .build();
+    }
 
-        client.setActivity(Activity.playing("Made in Java using JDA"));
-        client.setStatus(OnlineStatus.DO_NOT_DISTURB);
-        client.build();
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().equals("<@!889918153931517983>")) {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("da oi");
+        }
+    }
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        System.out.println("oie");
     }
 }
